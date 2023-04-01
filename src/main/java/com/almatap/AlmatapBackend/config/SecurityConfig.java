cdeb3af44ae21a3c555223c1291be9fafb075c90
AkzholLucky <slamjanovakjol@gmail.com>
2023-04-02 02:41:12 +0600

    just

--- src/main/java/com/almatap/AlmatapBackend/config/SecurityConfig.java
+++ src/main/java/com/almatap/AlmatapBackend/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable()
+        http.cors().and()
                 .csrf().disable().authorizeRequests()
                 .antMatchers("/adminPage/**").hasRole("ADMIN")
                 .antMatchers("/auth/**", "/error").permitAll()
